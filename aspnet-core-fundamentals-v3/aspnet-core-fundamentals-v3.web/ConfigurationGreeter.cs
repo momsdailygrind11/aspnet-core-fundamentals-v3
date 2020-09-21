281e0e88cc7d14892dede494d06219b885785c88
momsdailygrind11 <momsdailygrind11@hotmail.com>
2020-09-21 01:12:36 -0500

    Step 005

---
+++ aspnet-core-fundamentals-v3/aspnet-core-fundamentals-v3.web/ConfigurationGreeter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnet_core_fundamentals_v3.web
+{
+    public class ConfigurationGreeter : IGreeter
+    {
+        public IConfiguration Configuration { get; }
+        public ConfigurationGreeter(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public string GetGreeting()
+        {
+            return Configuration["Greeting"];
+        }
+
+
+    }
+}
--- aspnet-core-fundamentals-v3/aspnet-core-fundamentals-v3.web/Program.cs
+++ aspnet-core-fundamentals-v3/aspnet-core-fundamentals-v3.web/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using aspnet_core_fundamentals_v3.web;
