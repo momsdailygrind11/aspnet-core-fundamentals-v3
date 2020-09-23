412c0231e40ac287f30031d26287c12dfad7498c
momsdailygrind11 <momsdailygrind11@hotmail.com>
2020-09-23 01:02:54 -0500

    Middlewear steps

---
+++ aspnet-core-fundamentals-v3/aspnet-core-fundamentals-v3.web/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnet_core_fundamentals_v3.web.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Hello from a controller";
+        }
+    }
+}
--- aspnet-core-fundamentals-v3/aspnet-core-fundamentals-v3.web/Startup.cs
+++ aspnet-core-fundamentals-v3/aspnet-core-fundamentals-v3.web/Startup.cs
@@ -18,6 +18,8 @@ namespace aspnet_core_fundamentals_v3.web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddMvc();
             services.AddSingleton<IGreeter, ConfigurationGreeter>();
         }
 
@@ -28,16 +30,42 @@ namespace aspnet_core_fundamentals_v3.web
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler(new ExceptionHandlerOptions
+                    {
+                    ExceptionHandler = context =>
+                    
+                        context.Response.WriteAsync("You went the wrong way!")
+                    
+                });
+            }
+
+            // app.UseFileServer();
+
+            app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseWelcomePage(new WelcomePageOptions { 
+                Path = "/welcome"
+            });
+
+           
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //       // throw new Exception("Something went wrong");   
+            //        var message = greeter.GetGreeting();
+            //        await context.Response.WriteAsync(message);
+            //    });
+            //});
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    var message = greeter.GetGreeting();
-                    await context.Response.WriteAsync(message);
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
