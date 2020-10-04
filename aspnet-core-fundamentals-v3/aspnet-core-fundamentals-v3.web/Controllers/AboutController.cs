7f1ac1d914ced22ac62c135a307d3002e8a7af29
momsdailygrind11 <momsdailygrind11@hotmail.com>
2020-10-03 19:44:48 -0500

    Step11

---
+++ aspnet-core-fundamentals-v3/aspnet-core-fundamentals-v3.web/Controllers/AboutController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace aspnet_core_fundamentals_v3.web.Controllers
+{
+    [Route("About")]
+
+    public class AboutController
+    {
+        [Route("ph")]
+        public string Phone()
+        {
+            return "636-900-2497";
+        }
+
+        [Route("add")]
+        public string Address()
+        {
+            return "USA";
+        }
+    }
+   
+}
