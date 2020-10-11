06d66eaea7d02d1f39a1e7d1e0c01145ebeaff69
momsdailygrind11 <momsdailygrind11@hotmail.com>
2020-10-11 01:13:56 -0500

    Step21

---
+++ aspnet-core-fundamentals-v3/SimpleCrm.SqlDbContext/SimpleCrmDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using aspnet_core_fundamentals_v3.web;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleCrm.SqlDbServices
+{
+    class SimpleCrmDbContext : DbContext
+    {
+        public SimpleCrmDbContext(DbContextOptions<SimpleCrmDbContext> options)
+            : base(options)   { }
+            public DbSet<Customer> Customers { get; set; }
+        
+    }
+}
---
+++ aspnet-core-fundamentals-v3/SimpleCrm.SqlDbContext/SqlCustomerData.cs
@@ -0,0 +1,34 @@
+﻿using aspnet_core_fundamentals_v3.web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace SimpleCrm.SqlDbServices
+{
+    class SqlCustomerData : ICustomerData
+    {
+        private SimpleCrmDbContext _context;
+        public SqlCustomerData(SimpleCrmDbContext context)
+        {
+            _context = context;
+        }
+
+        public Customer Get(int id)
+        {
+            return _context.Customers.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            return _context.Customers.ToList();
+        }
+
+        public void Save(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+        }
+    }
+}
