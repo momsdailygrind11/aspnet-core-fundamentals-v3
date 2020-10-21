01e858a14179b10797ee0d88bbd1608de45ed921
momsdailygrind11 <momsdailygrind11@hotmail.com>
2020-10-21 00:16:39 -0500

    Step23

--- aspnet-core-fundamentals-v3/SimpleCrm/Customer.cs
+++ aspnet-core-fundamentals-v3/SimpleCrm/Customer.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SimpleCrm
 {
     public class Customer
     {
         public int Id { get; set; }
+        [MaxLength(50)] 
+        [Required()] 
         public string FirstName { get; set; }
+        [MinLength(1), MaxLength(50)] 
+        [Required()] 
         public string LastName { get; set; }
+        [MinLength(7), MaxLength(12)] 
         public string PhoneNumber { get; set; }
-
-        public CustomerType Type { get; set; }
-        
-        public bool OptInNewsletter { get; set; }
+        public bool OptInNewsletter { get; set; } 
+        public CustomerType Type { get; set; } 
     }
 }
