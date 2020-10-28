b2b30e57e266fb3687bb6bbb95e6235f82addc2b
momsdailygrind11 <momsdailygrind11@hotmail.com>
2020-10-28 01:27:09 -0500

    Step30

---
+++ aspnet-core-fundamentals-v3/SimpleCrm.Web/ViewComponents/GreetingViewComponents.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace SimpleCrm.Web.ViewComponents
+{
+	public class GreetingViewComponent : ViewComponent
+	{
+		private readonly IGreeter greeter;
+
+		public GreetingViewComponent(IGreeter greeter)
+        {
+			this.greeter = greeter;
+        }
+		public Task<IViewComponentResult> InvokeAsync()
+		{
+			var model = greeter.GetGreeting();
+			return Task.FromResult<IViewComponentResult>(View("Default", model));
+		}
+	}
+}
