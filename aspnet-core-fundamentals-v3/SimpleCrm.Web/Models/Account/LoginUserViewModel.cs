6e7dfb5e2272b5bc7985228d93e80decca1feefd
momsdailygrind11 <momsdailygrind11@hotmail.com>
2020-11-08 17:00:42 -0600

    Step36

---
+++ aspnet-core-fundamentals-v3/SimpleCrm.Web/Models/Account/LoginUserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCrm.Web.Models.Account
+{
+    public class LoginUserViewModel
+    {
+        [Required, MaxLength(256), DisplayName("Email Address"), DataType(DataType.EmailAddress)]
+        public string UserName { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ aspnet-core-fundamentals-v3/SimpleCrm.Web/ViewComponents/LoginLogoutViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+
+namespace SimpleCrm.Web.ViewComponents
+{
+    public class LoginLogoutViewComponent : ViewComponent
+    {
+        private readonly UserManager<CrmUser> userManager;
+        public LoginLogoutViewComponent(UserManager<CrmUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = userManager.Users
+                .FirstOrDefault(x => x.UserName == User.Identity.Name);
+            return Task.FromResult<IViewComponentResult>(View(user));
+        }
+    }
+}
