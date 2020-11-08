d94e13df4f808a06900591d384d90ed11538b809
momsdailygrind11 <momsdailygrind11@hotmail.com>
2020-11-07 22:44:36 -0600

    Step32

---
+++ aspnet-core-fundamentals-v3/SimpleCrm.SqlDbServices/CrmIdentityDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleCrm.SqlDbServices
+{
+    public class CrmIdentityDbContext : IdentityDbContext<CrmUser>
+    {
+        public CrmIdentityDbContext(DbContextOptions<CrmIdentityDbContext> options)
+              : base(options)
+        {
+        }
+    }
+}
---
+++ aspnet-core-fundamentals-v3/SimpleCrm/CrmUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace SimpleCrm
+{
+    public class CrmUser : IdentityUser
+    {
+        [MaxLength(256)]
+        public string DisplayName { get; set; }
+    }
+}
