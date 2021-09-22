e14f25ce7296139189d1573de98e2442111df3aa
Felicia Fowler <momsdailygrind11@hotmail.com>
2021-09-21 21:27:30 -0500

    Step6

--- SimpleCrm/SimpleCrm.Web/Startup.cs
+++ SimpleCrm/SimpleCrm.Web/Startup.cs
@@ -29,6 +29,8 @@ namespace SimpleCrm.Web
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseWelcomePage(new WelcomePageOptions{  Path = "/welcome" });
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
